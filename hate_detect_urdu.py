7e7f6df4206b68e50729dc159382535797a4b503
codteem <codteem@gmail.com>
2022-04-21 00:03:58 +0500

    swapping the hate_speech_urdu and streamlit_app
    swapping the hate_speech_urdu and streamlit_app

--- hate_detect_urdu.py
+++ hate_detect_urdu.py
@@ -1,10 +1,36 @@
 from collections import namedtuple
 import altair as alt
+import math
 import pandas as pd
 import streamlit as st
+
 import numpy as np
 import pickle
 from PIL import Image
+
+with st.echo(code_location='below'):
+    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+
+    Point = namedtuple('Point', 'x y')
+    data = []
+
+    points_per_turn = total_points / num_turns
+
+    for curr_point_num in range(total_points):
+        curr_turn, i = divmod(curr_point_num, points_per_turn)
+        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+        radius = curr_point_num / total_points
+        x = radius * math.cos(angle)
+        y = radius * math.sin(angle)
+        data.append(Point(x, y))
+
+    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+        .mark_circle(color='#0068c9', opacity=0.5)
+        .encode(x='x:Q', y='y:Q'))
+
+
+
 
 # pickle_in = open("classifier.pkl","rb")
 # classifier=pickle.load(pickle_in)
@@ -18,20 +44,24 @@ def predict_note_authentication(variance,skewness,curtosis,entropy):
     return prediction
 
 def main():
-    st.title("Toxicity Detection Urdu")
+    st.title("Bank Authenticator")
     html_temp = """
     <div style="background-color:tomato;padding:10px">
     <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
     </div>
     """
     st.markdown(html_temp,unsafe_allow_html=True)
-    urdu_text = st.text_input("Urdu Text","Type Here")
-
+    variance = st.text_input("Variance","Type Here")
+    skewness = st.text_input("skewness","Type Here")
+    curtosis = st.text_input("curtosis","Type Here")
+    entropy = st.text_input("entropy","Type Here")
     result=""
-    
-    if st.button("Predict"):
-        # result=predict_note_authentication(variance,skewness,curtosis,entropy)
-    st.success('The output is {}'.format(result))
+    # if st.button("Predict"):
+    #     result=predict_note_authentication(variance,skewness,curtosis,entropy)
+    # st.success('The output is {}'.format(result))
+    if st.button("About"):
+        st.text("Lets LEarn")
+        st.text("Built with Streamlit")
 
 if __name__=='__main__':
     main()   
