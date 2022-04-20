3c6c68c5c6bda9a6e09904ad5b5749501c8fc732
codteem <codteem@gmail.com>
2022-04-21 00:00:29 +0500

    Adding Toxicity Headings
    Adding Toxicity Headings

---
+++ hate_detect_urdu.py
@@ -0,0 +1,38 @@
+from collections import namedtuple
+import altair as alt
+import pandas as pd
+import streamlit as st
+import numpy as np
+import pickle
+from PIL import Image
+
+# pickle_in = open("classifier.pkl","rb")
+# classifier=pickle.load(pickle_in)
+
+def welcome():
+    return "Welcome All"
+
+def predict_note_authentication(variance,skewness,curtosis,entropy):
+    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    print(prediction)
+    return prediction
+
+def main():
+    st.title("Toxicity Detection Urdu")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    urdu_text = st.text_input("Urdu Text","Type Here")
+
+    result=""
+    
+    if st.button("Predict"):
+        # result=predict_note_authentication(variance,skewness,curtosis,entropy)
+    st.success('The output is {}'.format(result))
+
+if __name__=='__main__':
+    main()   
+    
