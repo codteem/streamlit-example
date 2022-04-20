7a908e722ba5e1f2cbd2b9801280dc9d75963704
codteem <codteem@gmail.com>
2022-04-20 23:54:58 +0500

    Adding four Text Entries
    Adding four Text Entries

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,9 @@ import math
 import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
+import numpy as np
+import pickle
+from PIL import Image
 
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
@@ -36,3 +28,41 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+
+
+
+
+# pickle_in = open("classifier.pkl","rb")
+# classifier=pickle.load(pickle_in)
+
+def welcome():
+    return "Welcome All"
+
+def predict_note_authentication(variance,skewness,curtosis,entropy):
+    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    print(prediction)
+    return prediction
+
+def main():
+    st.title("Bank Authenticator")
+    html_temp = """
+    <div style="background-color:tomato;padding:10px">
+    <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
+    </div>
+    """
+    st.markdown(html_temp,unsafe_allow_html=True)
+    variance = st.text_input("Variance","Type Here")
+    skewness = st.text_input("skewness","Type Here")
+    curtosis = st.text_input("curtosis","Type Here")
+    entropy = st.text_input("entropy","Type Here")
+    result=""
+    # if st.button("Predict"):
+    #     result=predict_note_authentication(variance,skewness,curtosis,entropy)
+    # st.success('The output is {}'.format(result))
+    if st.button("About"):
+        st.text("Lets LEarn")
+        st.text("Built with Streamlit")
+
+if __name__=='__main__':
+    main()   
+    
